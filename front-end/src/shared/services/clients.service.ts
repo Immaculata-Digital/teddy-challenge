6c520118cd1a141df61cb14e8e44761f8db3c6d7
Macbook <macbook@Macbooks-MacBook-Air.local>
2026-04-29 00:56:29 -0300

    fix: atualiza clients.service.ts para usar url dinâmica da api

--- front-end/src/shared/services/clients.service.ts
+++ front-end/src/shared/services/clients.service.ts
@@ -2,7 +2,7 @@ import { IClient, IPagination, CreateClientDto, UpdateClientDto } from '@teddy/s
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api/v1',
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000/api/v1',
   withCredentials: true,
 });
 
